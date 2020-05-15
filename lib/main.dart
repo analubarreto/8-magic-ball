1f3c2256327d54272955407b0a981895bc8bd074
analubarreto <analuibm@gmail.com>
2020-05-15 16:28:36 -0300

    finished app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,58 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+          home: Scaffold(
+              backgroundColor: Colors.white,
+              appBar: AppBar(
+                title: Text(
+                  'Magic 8 Ball',
+                  style: TextStyle(color: Colors.black38),
+                ),
+                backgroundColor: Colors.white,
+              ),
+              body: MagicBallPage())),
+    );
+
+class MagicBallPage extends StatefulWidget {
+  @override
+  _MagicBallPageState createState() => _MagicBallPageState();
+}
+
+class _MagicBallPageState extends State<MagicBallPage> {
+  int magicBallAsw = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Ask me anything: ',
+            style: TextStyle(
+                color: Colors.red, fontSize: 40.0, fontWeight: FontWeight.w300),
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      magicBallAsw = Random().nextInt(5) + 1;
+                    });
+                  },
+                  child: Image.asset('images/ball$magicBallAsw.png'),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
+  }
+}
